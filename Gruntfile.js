12181579f87133cd06952aee005e8cb10fffbf92
Michael Grosse <grosse@cosmocode.de>
2016-09-25 11:16:28 +0200

    Add grunt for linting and less compilation
    This counts toward #1

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      less: {
+          development: {
+              files: {
+                  "data/css/sidebar.css": "data/css/sidebar.less"
+              },
+              options: {}
+          }
+      },
+      jshint: {
+          all: ['Gruntfile.js', 'index.js', 'data/**/*.js', 'test/**/*.js']
+      }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+};
