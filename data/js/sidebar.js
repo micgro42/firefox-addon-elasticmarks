8927f86522c868beec8aa4ee74ed1d6d46f7449c
Michael Grosse <grosse@cosmocode.de>
2016-09-25 11:22:18 +0200

    Extract tld from urls and show as checkboxes
    This is a step towards implementing domain-filters: #5

--- data/js/sidebar.js
+++ data/js/sidebar.js
@@ -6,7 +6,16 @@ addon.port.on("queryResults", function(results) {
     while (resultsList.firstChild) {
         resultsList.removeChild(resultsList.firstChild);
     }
+    var domains = {};
     results.forEach(function(value, index) {
+        console.dir(value);
+        var tld = new URL(value.url).hostname.split(".").pop();
+        if (!domains[tld]) {
+            domains[tld] = {
+                count: 0
+            };
+        }
+        domains[tld].count += 1;
         var link = document.createElement("A");
         link.href = value.url;
         link.appendChild(document.createTextNode(value.title));
@@ -14,6 +23,25 @@ addon.port.on("queryResults", function(results) {
         node.appendChild(link);
         resultsList.appendChild(node);
     });
+    var fsdomains = document.getElementById('fsdomains');
+    while (fsdomains.lastChild) {
+        if (fsdomains.lastChild.tagName == "LEGEND") {
+            break;
+        }
+        fsdomains.removeChild(fsdomains.lastChild);
+    }
+    domainList = Object.keys(domains);
+    domainList.forEach(function(domainname, index) {
+        console.dir(domainname);
+        var cb = document.createElement("INPUT");
+        cb.type = "checkbox";
+        cb.title = "filter by ." + domainname;
+        var label = document.createElement("LABEL");
+        label.appendChild(document.createTextNode(domainname+" ("+domains[domainname].count+")"));
+        label.appendChild(cb);
+        fsdomains.appendChild(label);
+    });
+
 });
 
 var search = document.getElementById('bmsearch');
