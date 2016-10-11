7ea16598c3b732cec208991bf28d86e8bfc29e64
Michael Grosse <grosse@cosmocode.de>
2016-10-11 18:44:27 +0200

    Adjust linting by eslint and add it to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,10 +28,13 @@ module.exports = function(grunt) {
             },
             all: ['Gruntfile.js', 'index.js', 'data/**/*.js', 'test/**/*.js']
         },
+        eslint: {
+            all: ['<%= jshint.all %>']
+        },
         watch: {
             js: {
                 files: ['<%= jshint.all %>'],
-                tasks: ['jshint']
+                tasks: ['jshint', 'eslint']
             },
             less: {
                 files: ['data/css/*.less'],
@@ -43,4 +46,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-eslint');
 };
