d1bc1145cf02c76b3fea5420c3c0e9ae29a03f1e
Michael Grosse <grosse@cosmocode.de>
2016-10-23 19:27:50 +0200

    Add class to filter bookmark by aspect

--- data/js/functions.js
+++ data/js/functions.js
@@ -1,6 +1,29 @@
 'use strict';
 var elasticmarks = elasticmarks || {};
 
+elasticmarks.Filter = class Filter {
+    construct (aspect, relation, value) {
+        this.aspect = aspect;
+        this.relation = relation;
+        this.value = value;
+        return this;
+    }
+    isBookmarkValid (bookmark) {
+        if (typeof bookmark.parts == 'undefined') {
+            bookmark.parts = elasticmarks.getDomainParts(bookmark.url);
+        }
+        if (typeof bookmark[this.aspect] != 'undefined') {
+            return bookmark[this.aspect] === this.value;
+        }
+        if (typeof bookmark.parts[this.aspect] != 'undefined') {
+            return bookmark.parts[this.aspect] === this.value;
+        }
+        console.error('Aspect ' + this.aspect + ' is unknown!');
+        return null;
+    }
+};
+
+
 /**
  * @todo: write tests, get protocoll
  */
