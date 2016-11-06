b43df5fec41ddf0260c573d09229b3c367d9ebe0
Michael Grosse <grosse@cosmocode.de>
2016-11-06 19:05:39 +0100

    refactor: remove unused filter class & its test
    Use small and simple functions for filtering instead

--- data/js/functions.js
+++ data/js/functions.js
@@ -1,27 +1,6 @@
 'use strict';
 var elasticmarks = elasticmarks || {};
 
-elasticmarks.Filter = class Filter {
-    construct (aspect, relation, value) {
-        this.aspect = aspect;
-        this.relation = relation;
-        this.value = value;
-        return this;
-    }
-    isBookmarkValid (bookmark) {
-        if (typeof bookmark.parts == 'undefined') {
-            bookmark.parts = elasticmarks.getDomainParts(bookmark.url);
-        }
-        if (typeof bookmark[this.aspect] != 'undefined') {
-            return bookmark[this.aspect] === this.value;
-        }
-        if (typeof bookmark.parts[this.aspect] != 'undefined') {
-            return bookmark.parts[this.aspect] === this.value;
-        }
-        console.error('Aspect ' + this.aspect + ' is unknown!');
-        return null;
-    }
-};
 
 elasticmarks.tagCounter = function (carrier, bookmark) {
     return bookmark.tags.reduce(function(carrier, tag) {
--- data/test/filtering.test.js
+++ data/test/filtering.test.js
@@ -1,21 +1,5 @@
 'use strict';
 var elasticmarks = elasticmarks || {};
 
-QUnit.module( 'filterBookmarks', {
-    beforeEach: function( ) {
-    }, afterEach: function( ) {
-    }
-});
-QUnit.test( '1 filter: tld valid', function( assert ) {
-    var tldfilter = new elasticmarks.Filter().construct('tld', 'equal', 'org');
-    var bookmark = {
-        id: 8,
-        index: 0,
-        title: 'Help and Tutorials',
-        updated: 1477241737303000,
-        url: 'https://www.mozilla.org/en-US/firefox/help/',
-    };
-    assert.strictEqual(tldfilter.isBookmarkValid(bookmark), true);
-});
 
 
